34dc39ac8aeb16e7553bf7f9a496bfe4dcd90535
Philipp Sossong <Psossong@gmx.de>
2018-11-18 11:09:52 +0100

    Einfügen einer CalenderActivity, Setting für Debugging mittels Handy gestartet

--- app/src/main/java/psdevelop/liveplaner/MainActivity.java
+++ app/src/main/java/psdevelop/liveplaner/MainActivity.java
@@ -1,13 +1,25 @@
 package psdevelop.liveplaner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button CalButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CalButton = findViewById(R.id.CalenderButton);
+        CalButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Calender.class));
+            }
+        });
     }
 }
